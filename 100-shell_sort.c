f7fbcccd9147b0df937fdfe87d7ec0a083ca3f36
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2024-01-23 21:33:48 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,11 +1,5 @@
 #include "sort.h"
 
-/**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
 void swap(int *array, int item1, int item2)
 {
 
@@ -15,12 +9,7 @@ void swap(int *array, int item1, int item2)
 	array[item1] = array[item2];
 	array[item2] = tmp;
 }
-/**
- * shell_sort - function that sorts an array of integers in ascending
- * order using the Shell sort algorithm, using the Knuth sequence
- * @size: size of the array
- * @array: list with numbers
- */
+
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i, index = 0;
