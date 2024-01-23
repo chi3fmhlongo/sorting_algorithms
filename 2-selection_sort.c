93efb87bbe68c226d2ade160aeca35ce10f61197
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2024-01-23 21:38:14 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,6 @@
 #include "sort.h"
-/**
- * selection_sort - function that sorts an array of integers in ascending
- * order using the Selection sort algorithm
- * @size: size of the array
- * @array: list with numbers
+/*
+ * selection_sort
  */
 void selection_sort(int *array, size_t size)
 {
